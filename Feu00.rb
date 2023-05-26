9ec8316c3e4c2a491f69a2987ba9aa1f4ac5b967
RedonJJ <jjredon.dev@gmail.com>
2023-05-26 13:00:05 +0200

    ajout de l'exercice Feu01.rb + application des bonnes pratiques

--- Feu00.rb
+++ Feu00.rb
@@ -4,10 +4,10 @@ width = ARGV[0].to_i
 height = ARGV[1].to_i
 
 # Méthode
-def drawLine(width, is_top_or_bottom)
-    if width == 1
+def draw_line(width, is_top_or_bottom)
+    if width == 1 then
         puts 'o'
-    elsif is_top_or_bottom
+    elsif is_top_or_bottom then
         puts 'o' + '-' * (width - 2) + 'o'
     else 
         puts '|' + ' ' * (width - 2) + '|'
@@ -15,17 +15,17 @@ def drawLine(width, is_top_or_bottom)
 end
 
 # Erreur
-if width.zero? || height.zero?
+if width.zero? || height.zero? then
     puts 'Erreur'
     exit
 end
 
 # Affichage du rectangle
 height.times do |i|
-    if i.zero? || i == height - 1
-        drawLine(width, true)
+    if i.zero? || i == height - 1 then
+        draw_line(width, true)
     else
-        drawLine(width, false)
+        draw_line(width, false)
     end
 end
 # Fin de l'exercice
---
+++ Feu01.rb
@@ -0,0 +1,75 @@
+# Créez un programme qui reçoit une expression arithmétique dans une chaîne de caractères
+# et en retourne le résultat après l’avoir calculé.
+
+# Vous devez gérer les 5 opérateurs suivants : “+” pour l’addition, “-” pour la soustraction,
+# “*” la multiplication, “/” la division et “%” le modulo.
+
+class ArithmeticExpression
+    def evaluate(expression)
+        tokens = tokenize(expression)
+        result = parse_expression(tokens)
+        raise "Expression invalide: #{expression}" if tokens.any?
+        result 
+    end
+  
+    def parse_expression(tokens)
+        result = parse_term(tokens)
+        while tokens.first == '+' || tokens.first == '-'
+        operator = tokens.shift
+        right = parse_term(tokens)
+        if operator == '+' then
+          result += right
+        else 
+          result -= right
+        end
+      end
+      result
+    end
+  
+    def parse_term(tokens)
+      result = parse_factor(tokens)
+      while tokens.first == '*' || tokens.first == '/' || tokens.first == '%'
+        operator = tokens.shift
+        right = parse_factor(tokens)
+        if operator == '*' then
+          result *= right
+        elsif operator == '/' then
+          result /= right
+        elsif operator == '%' then
+          result %= right
+        end
+      end
+      result 
+    end
+  
+    def parse_factor(tokens)
+      if tokens.first == '(' then
+        tokens.shift
+        result = parse_expression(tokens)
+        raise "Expected ')'" if tokens.shift != ')'
+        result
+      elsif tokens.first =~ /\A[+-]?\d+\z/ then
+        tokens.shift.to_i
+      else 
+        raise "Unexpected token: #{tokens.first}"
+      end
+    end
+  
+    def tokenize(expression)
+      expression.scan(/\d+|[+\-*\/%()]|\S+/)
+    end
+  end
+  
+  unless ARGV.length == 1
+    puts 'Erreur: nombre incorrect d\'arguments'
+    puts "Utilisation: ruby #{__FILE__} \"expression\""
+    exit
+  end
+  
+  expression = ARGV[0]
+  
+  arithmetic_expression = ArithmeticExpression.new
+  
+  result = arithmetic_expression.evaluate(expression)
+  puts result
+#Fin de l'exercice.
